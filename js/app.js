4eac58497e92a37342488e937a45be16e33a1826
makintosh1337 <zxcirclet@outlook.com>
2025-03-01 14:18:31 +0700

    TaskCard

--- js/app.js
+++ js/app.js
@@ -1,3 +1,65 @@
+const TaskCard = {
+    props: {
+        taskTitle: String,
+        taskItems: Array,
+        taskColumn: Number,
+        taskIndex: Number,
+        completionTime: String,
+        relocateTask: Function,
+        modifyTask: Function,
+        secondColumnTaskCount: Number,
+        restrictFirstColumn: Boolean,
+    },
+    template: `
+        <div class="task-card">
+            <h3>{{ taskTitle }}</h3>
+            <ul>
+                <li v-for="(item, index) in taskItems" :key="index">{{ item }}</li>
+            </ul>
+            <button @click="moveTask">Переместить</button>
+            <button @click="editTask">Редактировать</button>
+        </div>
+    `,
+    methods: {
+        moveTask() {
+            this.relocateTask(this.taskColumn, this.taskIndex);
+        },
+        editTask() {
+            this.modifyTask(this.taskColumn, this.taskIndex);
+        },
+    },
+};
+
+const TaskColumn = {
+    props: {
+        columnID: Number,
+        taskCards: Array,
+        relocateTask: Function,
+        modifyTask: Function,
+        secondColumnTaskCount: Number,
+        restrictFirstColumn: Boolean,
+    },
+    components: { TaskCard },
+    template: `
+        <div class="task-column">
+            <h2>Столбец {{ columnID }}</h2>
+            <div v-for="(card, index) in taskCards" :key="index">
+                <TaskCard
+                  :taskTitle="card.title"
+                  :taskItems="card.list"
+                  :taskColumn="columnID"
+                  :taskIndex="index"
+                  :completionTime="card.completedAt"
+                  :relocateTask="relocateTask"
+                  :modifyTask="modifyTask"
+                  :secondColumnTaskCount="secondColumnTaskCount"
+                  :restrictFirstColumn="restrictFirstColumn"
+                />
+            </div>
+        </div>
+    `,
+};
+
 const taskManager = new Vue({
     el: '#app',
     data() {
@@ -16,7 +78,38 @@ const taskManager = new Vue({
         };
     },
     methods: {
-
+        addTask() {
+            if (this.newTask.title && this.newTask.list.every(item => item.trim() !== '')) {
+                this.taskColumns[0].taskCards.push({ ...this.newTask });
+                this.newTask = { title: '', list: ['', '', ''], completedAt: null };
+                this.saveToLocalStorage();
+            }
+        },
+        addTaskItem() {
+            if (this.newTask.list.length < 5) {
+                this.newTask.list.push('');
+            }
+        },
+        removeTaskItem(index) {
+            if (this.newTask.list.length > 3) {
+                this.newTask.list.splice(index, 1);
+            }
+        },
+        relocateTask(fromColumn, taskIndex) {
+            if (fromColumn < this.taskColumns.length) {
+                const task = this.taskColumns[fromColumn].taskCards.splice(taskIndex, 1)[0];
+                this.taskColumns[fromColumn + 1].taskCards.push(task);
+                this.saveToLocalStorage();
+            }
+        },
+        modifyTask(columnID, taskIndex) {
+            const task = this.taskColumns[columnID].taskCards[taskIndex];
+        },
+        saveToLocalStorage() {
+            this.taskColumns.forEach((column, index) => {
+                localStorage.setItem(`column${index + 1}`, JSON.stringify(column.taskCards));
+            });
+        },
     },
     components: { TaskColumn },
     template: `
@@ -42,7 +135,8 @@ const taskManager = new Vue({
                             :taskCards="column.taskCards"
                             :relocateTask="relocateTask"
                             :modifyTask="modifyTask"
-                            :secondColumnTaskCount="taskColumns[1].taskCards.length" />
+                            :secondColumnTaskCount="taskColumns[1].taskCards.length"
+                            :restrictFirstColumn="restrictFirstColumn" />
             </div>
         </div>
     `,
